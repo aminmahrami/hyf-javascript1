0cd06ad31eb2a2c6c06be9c78d8503baf59ef7ee
aminmahrami <31386996+aminmahrami@users.noreply.github.com>
2017-09-14 00:31:48 +0200

    Add files via upload

---
+++ week1/index.js
@@ -0,0 +1,31 @@
+//variable x 
+let x = 7;
+
+//rounded of 7.25, to the nearest integer
+Math.round(7.23);// rounded = 7
+
+let colors =["red","green","blue"]; //array called colors with the strings
+//length of the string
+console.log(colors.length); // length = 3 
+
+//checks the types of two variables
+let x = 9;
+let y = 'Hello';
+if(x==y){
+  (console.log('SAME TYPE'))
+};
+if(x!=y){
+  (console.log('NOT SAME TYPE'))
+};
+
+let items = ["soap","gel",5,"blade"]; //multiple types in an array
+
+// the new value of x
+x = x % 3;
+//statement is x = x % 3;>> new x = 1
+
+//compare inifities
+let x = 6/0;
+let y = 10/0;
+if(x==y)(console.log('EQUAL'));
+if(x!=y)(console.log('NOT EQUAL'));// EQUAL
